a5f82a8b424b99c34cb22befddcf7744e3fd81f5
Betsy Haibel <betsy@transparentclassroom.com>
2022-07-27 10:00:18 -0700

    add warning for folks using the latest Rails security patches

--- lib/lab_tech/engine.rb
+++ lib/lab_tech/engine.rb
@@ -2,5 +2,21 @@ module LabTech
   class Engine < ::Rails::Engine
     isolate_namespace LabTech
     config.generators.api_only = true
+
+    config.after_initialize do
+      required_serializable_classes = [
+        ActiveSupport::Duration,
+        ActiveSupport::TimeWithZone,
+        ActiveSupport::TimeZone,
+        Time,  
+      ]
+      
+      missing_classes = required_serializable_classes - Rails.configuration.active_record.yaml_column_permitted_classes
+
+      if missing_classes.any?
+        puts "Please add #{missing_classes.join(', ')} to your Rails.configuration.active_record.yaml_column_permitted_classes.".red      
+        puts "LabTech will break your application horribly unless you do.".red      
+      end
+    end
   end
 end
